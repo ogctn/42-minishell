7c8e95279c58c0f81b377f9febbf97f23638a3de
Sevgi Gundogdu <sgundogd@k1m31s07.42istanbul.com.tr>
2023-11-16 17:25:37 +0300

    export bug fixed

--- src/builtin/ft_export.c
+++ src/builtin/ft_export.c
@@ -6,11 +6,22 @@
 /*   By: sgundogd <sgundogd@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/04 00:04:22 by ogcetin           #+#    #+#             */
-/*   Updated: 2023/11/14 00:24:34 by sgundogd         ###   ########.fr       */
+/*   Updated: 2023/11/16 17:24:10 by sgundogd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/minishell.h"
+
+static int	find_len_content(char *content)
+{
+	int	len;
+
+	if (ft_strchr(content, '='))
+		len = ft_strchr(content, '=') - content;
+	else
+		len = ft_strlen(content);
+	return (len);
+}
 
 void	env_swap(t_env *a, t_env *b, int *swap_flag)
 {
@@ -94,14 +105,17 @@ void	print_export(t_env *env)
 int	is_env_exist(t_env *env, char *content)
 {
 	int	len_to_equal;
+	int	len_content;
 
+	len_content = find_len_content(content);
 	while (env)
 	{
 		if (ft_strchr(env->content, '='))
 			len_to_equal = ft_strchr(env->content, '=') - env->content;
 		else
 			len_to_equal = ft_strlen(env->content);
-		if (ft_strncmp(env->content, content, len_to_equal) == 0)
+		if (ft_strncmp(env->content, content, len_to_equal) == 0 \
+			&& len_to_equal == len_content)
 			return (1);
 		env = env->next;
 	}
@@ -111,14 +125,17 @@ int	is_env_exist(t_env *env, char *content)
 void	update_env_content(t_env **env, char *new)
 {
 	int	len_to_equal;
+	int	len_content;
 
+	len_content = find_len_content(new);
 	while (*env)
 	{
 		if (ft_strchr((*env)->content, '='))
 			len_to_equal = ft_strchr((*env)->content, '=') - (*env)->content;
 		else
 			len_to_equal = ft_strlen((*env)->content);
-		if (ft_strncmp((*env)->content, new, len_to_equal) == 0)
+		if (ft_strncmp((*env)->content, new, len_to_equal) == 0
+			&& len_content == len_to_equal)
 		{
 			if (ft_strchr((*env)->content, '=') && !ft_strchr(new, '='))
 				return ;
@@ -142,7 +159,7 @@ int	is_valid_export_parameter(char *str)
 {
 	int	i;
 
-	if (!str[0] || (!ft_isalpha(str[0]) && str[0] != '_'))
+	if (!str[0] || !(ft_isalpha(str[0]) || str[0] == '_'))
 	{
 		printf("minishell: export: `%s': not a valid identifier\n", str);
 		return (0);
